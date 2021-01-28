a3e204e7bb11e9d1f6f8a6d1c7a784570807d284
diwakarupmanyu <diwakarupmanyu@gmail.com>
2021-01-28 15:17:17 +0530

    first commit

---
+++ app/src/main/java/com/example/newrecyclerview/myviewholder.java
@@ -0,0 +1,18 @@
+package com.example.newrecyclerview;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class myviewholder extends RecyclerView.ViewHolder
+{
+    TextView t1,t2,t3;
+    public myviewholder(@NonNull View itemView) {
+        super(itemView);
+        t1=(TextView)itemView.findViewById(R.id.t1);
+        t2=(TextView)itemView.findViewById(R.id.t2);
+        t3=(TextView)itemView.findViewById(R.id.t3);
+    }
+}
